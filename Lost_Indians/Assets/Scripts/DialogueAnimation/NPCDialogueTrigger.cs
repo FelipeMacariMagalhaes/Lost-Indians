f6f3dcafac7704c8f2661c0755ae29e64988eadd
FelipeMacariMagalhaes <felipe.magalhaes@alunos.sc.senac.br>
2025-09-23 22:46:21 -0300

    DialogoFeito e exclui codigo interactionlight
    Codigo interaction nao havia funcionalidade

--- Lost_Indians/Assets/Scripts/DialogueAnimation/NPCDialogueTrigger.cs
+++ Lost_Indians/Assets/Scripts/DialogueAnimation/NPCDialogueTrigger.cs
@@ -2,29 +2,43 @@ using UnityEngine;
 
 public class NPCDialogueTrigger : MonoBehaviour
 {
-    [Header("Interação")]
-    public GameObject pressEUI; 
-
+   [Header("Interação")]
+    public GameObject pressEUI;   
+    public Vector3 offset = new Vector3(0, 1f, 0); 
+ 
     [Header("Diálogo")]
     public string[] dialogueLines;
     public string npcName = "NPC";
-
+ 
     private bool playerInRange = false;
-
+    private Camera cam;
+ 
     void Start()
     {
+        cam = Camera.main;
+ 
         if (pressEUI != null)
             pressEUI.SetActive(false);
     }
-
+ 
     void Update()
     {
-        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        if (playerInRange)
         {
-            DialogueManager.instance.StartDialogue(dialogueLines, npcName);
+            // Atualiza a posição do texto na tela (fixo acima da cabeça)
+            if (pressEUI != null)
+            {
+                Vector3 screenPos = cam.WorldToScreenPoint(transform.position + offset);
+                pressEUI.transform.position = screenPos;
+            }
+ 
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                DialogueManager.instance.StartDialogue(dialogueLines, npcName);
+            }
         }
     }
-
+ 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
@@ -34,7 +48,7 @@ public class NPCDialogueTrigger : MonoBehaviour
                 pressEUI.SetActive(true);
         }
     }
-
+ 
     void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
