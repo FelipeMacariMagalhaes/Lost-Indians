64aa05b98a593312dc599e8cabef56a860a97ade
Yuri Scheunemann <yuri.scheunemann@alunos.sc.senac.br>
2025-10-07 15:52:03 -0300

    Cutscene_restart

--- Lost_Indians/Assets/Scripts/Livros.cs
+++ Lost_Indians/Assets/Scripts/Livros.cs
@@ -4,9 +4,18 @@ using UnityEngine.Timeline;
 
 public class Livros : CountBooks
 {
+    public PlayableDirector cutsceneDirector;
     private void Start()
     {
         CollectMessage = "Você Coletou Um livro";
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            cutsceneDirector.Play();
+        }
     }
 }
     
