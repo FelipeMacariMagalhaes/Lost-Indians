23eff02192f950df559a4934c3769f56caead211
Yuri Scheunemann <yuri.scheunemann@alunos.sc.senac.br>
2025-10-13 15:58:26 -0300

    Background_flashback1

--- Lost_Indians/Assets/Scripts/Livros.cs
+++ Lost_Indians/Assets/Scripts/Livros.cs
@@ -3,16 +3,23 @@ using UnityEngine;
 using UnityEngine.Events;
 public class Livros : CountBooks
 {
+    bool flashOn = false;
     public UnityEvent Flashbacks;
+    public UnityEvent NonFlashBacks;
     
     private void Start()
     {
         CollectMessage = "Você Coletou Um livro";
+        if (flashOn == false)
+        {
+            NonFlashBacks.Invoke();
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         Flashbacks.Invoke();
+        flashOn = true;      
     }
 }
     
