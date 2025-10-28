c1c1cbc8b6e084ae612f60907611466fa79a2de3
Yuri Scheunemann <yuri.scheunemann@alunos.sc.senac.br>
2025-10-28 16:08:42 -0300

    Exit_flashback_scriptmore teleportAntibug

--- Lost_Indians/Assets/Scripts/Talks_activate.cs
+++ Lost_Indians/Assets/Scripts/Talks_activate.cs
@@ -1,14 +1,19 @@
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.Events;
 public class Talks_activate : MonoBehaviour
 {
     public UnityEvent talk;
-
+    public GameObject player;
+    public Transform TeleporteFlash;
+   
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            talk.Invoke();
+            talk.Invoke();            
+            player.transform.position = TeleporteFlash.position;               
+            
         }
     }
 }
