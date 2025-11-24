a44242e08ce995c62ba8616b79ef724ecf59472f
YuriScheunemann <yuri.scheunemann@alunos.sc.senac.br>
2025-11-24 19:11:27 -0300

    Reapply "Reapply "Blend tree""
    This reverts commit 055773a6288012975a3f2363b2c6f4fc0e89a498.

--- Lost_Indians/Assets/Scripts/MoveTopDown.cs
+++ Lost_Indians/Assets/Scripts/MoveTopDown.cs
@@ -2,68 +2,22 @@ using UnityEngine;
 
 public class MoveTopDown : MonoBehaviour
 {
-    [Header("Configurações")]
-    public float moveSpeed = 5f;
-    public float ladderSpeed = 2.5f;//velocidade na escada
-    private float currentSpeed;
-    private Vector2 movement;
-    public Rigidbody2D rb;
-    public Animator animator;
-    private SpriteRenderer spriteRenderer;
-    private bool onLadder = false;
+    public float speed = 5f;
+    private Animator anim;
 
     void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        animator = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
 
-        if (onLadder)
-        {
-            movement = new Vector2(0, vertical).normalized;
-            currentSpeed = ladderSpeed;
-        }
-        else
-        {
-            movement = new Vector2(horizontal, vertical).normalized;
-            currentSpeed = moveSpeed;// volta para a velo normal
-        }
+        transform.Translate(new Vector3(h, v, 0) * speed * Time.deltaTime);
 
-        bool isMoving = movement != Vector2.zero;
-        animator.SetBool("EstaMovendo", isMoving);
-        animator.SetFloat("MoveEixoX", movement.x);
-        animator.SetFloat("MoveEixoY", movement.y);
-
-        if (isMoving)
-        {
-            animator.SetFloat("LastMoveX", movement.x);
-            animator.SetFloat("LastMoveY", movement.y);
-        }
-    }
-
-    void FixedUpdate()
-    {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Escada"))
-        {
-            onLadder = true;
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Escada"))
-        {
-            onLadder = false;
-        }
+        anim.SetFloat("Horizontal", h);
+        anim.SetFloat("Vertical", v);
     }
 }
