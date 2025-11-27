8a119b2aeb9e1a7f8fbec83934a5ab9c3208806d
YuriScheunemann <yuri.scheunemann@alunos.sc.senac.br>
2025-11-26 22:36:17 -0300

    ButtonScript

---
+++ Lost_Indians/Assets/Scripts/DialogueAnimation/Button_accept.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Button_accept : MonoBehaviour
+{
+    public NPCDialogueTrigger nPC;
+    public UnityEvent Quest1;
+    public UnityEvent Quest2;
+    public UnityEvent Quest3;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Click()
+    {
+        if (nPC.questStage == 0)
+        {
+            Quest1.Invoke();
+        }
+
+        else if (nPC.questStage == 1) 
+        { 
+            Quest2.Invoke();
+        }
+
+        else if (nPC.questStage == 2)
+        {
+            Quest3.Invoke();
+        } 
+    }
+}
