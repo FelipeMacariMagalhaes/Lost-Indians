8bbf0484e807d0dfc84376fdae494a8874027902
YuriScheunemann <yuri.scheunemann@alunos.sc.senac.br>
2025-11-26 23:23:42 -0300

    PositionNpcQuest2_Done

--- Lost_Indians/Assets/Scripts/Flashback/Exit_flashback.cs
+++ Lost_Indians/Assets/Scripts/Flashback/Exit_flashback.cs
@@ -4,7 +4,9 @@ public class Exit_flashback : MonoBehaviour
 {
     public UnityEvent exit_flash;
     public GameObject Player;
+    public GameObject NPC;
     public Transform Teleport_flashOut;
+    public Transform Teleport_NPC;
     public Movi_2 movi_Plataform;
     public MoveTopDown moviTopDown;
     public bool plataform;
@@ -15,6 +17,7 @@ public class Exit_flashback : MonoBehaviour
         {
             exit_flash.Invoke();
             Player.transform.position = Teleport_flashOut.transform.position;
+            NPC.transform.position = Teleport_NPC.transform.position;
             movi_Plataform.enabled = plataform;
             moviTopDown.enabled = topdown;
         }
